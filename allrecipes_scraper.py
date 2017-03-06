6b851aac63b9606d90a0d808b5bdf113ed39bbdd
Adrian0121 <Adrian@users.noreply.github.com>
2017-03-05 18:30:17 -0600

    get more directions, not final

--- allrecipes_scraper.py
+++ allrecipes_scraper.py
@@ -2,50 +2,73 @@
 from bs4 import BeautifulSoup
 import urllib
 
-# Base URL to start from, only change this one
-r = urllib.urlopen('http://allrecipes.com/recipes/80/main-dish/?internalSource=hub%20nav&referringId=256&referringContentType=recipe%20hub&referringPosition=3&linkName=hub%20nav%20exposed&clickId=hub%20nav%203').read()
-soup = BeautifulSoup(r)
+cuisines = ["http://allrecipes.com/recipes/739/healthy-recipes/diabetic",
+			"http://allrecipes.com/recipes/741/healthy-recipes/gluten-free",
+			"http://allrecipes.com/recipes/84/healthy-recipes",
+			"http://allrecipes.com/recipes/1232/healthy-recipes/low-calorie",
+			"http://allrecipes.com/recipes/1231/healthy-recipes/low-fat"]
 
-# Find all of the links on the main url specified above
-links = []
-for link in soup.find_all("a", href=True):
-	print link
-	if link.get("href").startswith("/recipe"):
-		links.append("http://allrecipes.com" + link.get("href"))
-	elif link.get("href").startswith("http://allrecipes.com/recipe"):
-		links.append(link.get("href"))
+for c in cuisines:
 
-all_ingredients = []
+	# # Base URL to start from, only change this one
+	# r = urllib.urlopen('http://allrecipes.com/recipes/80/main-dish/?internalSource=hub%20nav&referringId=256&referringContentType=recipe%20hub&referringPosition=3&linkName=hub%20nav%20exposed&clickId=hub%20nav%203').read()
+	# soup = BeautifulSoup(r)
 
-# Find as many recipe links as possible. Not all of them lead to 
-# recipes, so it's not 100% robust, it only follows links once, 
-# DOES NOT keep scraping until it finds a recipe.
+	# # Find all of the links on the main url specified above
+	# links = []
+	# for link in soup.find_all("a", href=True):
+	# 	print link
+	# 	if link.get("href").startswith("/recipe"):
+	# 		links.append("http://allrecipes.com" + link.get("href"))
+	# 	elif link.get("href").startswith("http://allrecipes.com/recipe"):
+	# 		links.append(link.get("href"))
 
-for link in links:
-	r = urllib.urlopen(link)
-	soup = BeautifulSoup(r)
-	ingredients = soup.find_all("span", class_="recipe-directions__list--item")
+	# all_ingredients = []
 
-	count = 1
-	for ing in ingredients:
- 		all_ingredients.append(str(count) + ". " + ing.get_text())
- 		count += 1
+	# # Find as many recipe links as possible. Not all of them lead to 
+	# # recipes, so it's not 100% robust, it only follows links once, 
+	# # DOES NOT keep scraping until it finds a recipe.
 
- 	all_ingredients.append("NEXT RECIPE")
+	# for link in links:
+	# 	r = urllib.urlopen(link)
+	# 	soup = BeautifulSoup(r)
+	# 	ingredients = soup.find_all("span", class_="recipe-directions__list--item")
 
-# Get rid of bullshit unicode characters. Copyright character was causing problems
-for ing in all_ingredients[:]:
-	for letter in ing:
-		try:
-			letter.encode("ascii")
-		except UnicodeEncodeError:
-			newing = ing.replace(letter, "")
-			all_ingredients[all_ingredients.index(ing)] = newing
+	# 	count = 1
+	# 	for ing in ingredients:
+	#  		all_ingredients.append(str(count) + ". " + ing.get_text())
+	#  		count += 1
 
-# Write Ingredients to file
-ing_file = open("ingredients.txt", "w")
+	#  	all_ingredients.append("NEXT RECIPE")
 
-for ing in all_ingredients:
-	ing_file.write(ing + "\n")
+	# # Get rid of bullshit unicode characters. Copyright character was causing problems
+	# for ing in all_ingredients[:]:
+	# 	for letter in ing:
+	# 		try:
+	# 			letter.encode("ascii")
+	# 		except UnicodeEncodeError:
+	# 			newing = ing.replace(letter, "")
+	# 			all_ingredients[all_ingredients.index(ing)] = newing
 
-ing_file.close()
+	# Write Ingredients to file
+	word = ""
+	idx = -1
+	while c[idx] != "/":
+		word += c[idx]
+		idx -= 1
+	word = word[::-1]
+
+	print word
+
+	# ing_file = open("allDirections.txt", "a")
+	# ing_file2 = open("Directions.txt", "a")
+	# ing_file3 = open(word + "Directions.txt", "w")
+
+	# for ing in all_ingredients:
+	# 	ing_file.write(ing + "\n")
+	# 	ing_file2.write(ing + "\n")
+	# 	ing_file3.write(ing + "\n")
+
+	# ing_file.close()
+	# ing_file2.close()
+	# ing_file3.close()
