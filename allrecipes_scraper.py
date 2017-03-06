1a1936ad7da8be31a0a75ab17656f0e31409ed5b
Adrian0121 <Adrian@users.noreply.github.com>
2017-03-05 19:35:41 -0600

    add even more directions

--- allrecipes_scraper.py
+++ allrecipes_scraper.py
@@ -10,45 +10,44 @@ cuisines = ["http://allrecipes.com/recipes/739/healthy-recipes/diabetic",
 
 for c in cuisines:
 
-	# # Base URL to start from, only change this one
-	# r = urllib.urlopen('http://allrecipes.com/recipes/80/main-dish/?internalSource=hub%20nav&referringId=256&referringContentType=recipe%20hub&referringPosition=3&linkName=hub%20nav%20exposed&clickId=hub%20nav%203').read()
-	# soup = BeautifulSoup(r)
+	# Base URL to start from, only change this one
+	r = urllib.urlopen('http://allrecipes.com/recipes/80/main-dish/?internalSource=hub%20nav&referringId=256&referringContentType=recipe%20hub&referringPosition=3&linkName=hub%20nav%20exposed&clickId=hub%20nav%203').read()
+	soup = BeautifulSoup(r)
 
-	# # Find all of the links on the main url specified above
-	# links = []
-	# for link in soup.find_all("a", href=True):
-	# 	print link
-	# 	if link.get("href").startswith("/recipe"):
-	# 		links.append("http://allrecipes.com" + link.get("href"))
-	# 	elif link.get("href").startswith("http://allrecipes.com/recipe"):
-	# 		links.append(link.get("href"))
+	# Find all of the links on the main url specified above
+	links = []
+	for link in soup.find_all("a", href=True):
+		if link.get("href").startswith("/recipe"):
+			links.append("http://allrecipes.com" + link.get("href"))
+		elif link.get("href").startswith("http://allrecipes.com/recipe"):
+			links.append(link.get("href"))
 
-	# all_ingredients = []
+	all_ingredients = []
 
-	# # Find as many recipe links as possible. Not all of them lead to 
-	# # recipes, so it's not 100% robust, it only follows links once, 
-	# # DOES NOT keep scraping until it finds a recipe.
+	# Find as many recipe links as possible. Not all of them lead to 
+	# recipes, so it's not 100% robust, it only follows links once, 
+	# DOES NOT keep scraping until it finds a recipe.
 
-	# for link in links:
-	# 	r = urllib.urlopen(link)
-	# 	soup = BeautifulSoup(r)
-	# 	ingredients = soup.find_all("span", class_="recipe-directions__list--item")
+	for link in links:
+		r = urllib.urlopen(link)
+		soup = BeautifulSoup(r)
+		ingredients = soup.find_all("span", class_="recipe-directions__list--item")
 
-	# 	count = 1
-	# 	for ing in ingredients:
-	#  		all_ingredients.append(str(count) + ". " + ing.get_text())
-	#  		count += 1
+		count = 1
+		for ing in ingredients:
+	 		all_ingredients.append(str(count) + ". " + ing.get_text())
+	 		count += 1
 
-	#  	all_ingredients.append("NEXT RECIPE")
+	 	all_ingredients.append("NEXT RECIPE")
 
-	# # Get rid of bullshit unicode characters. Copyright character was causing problems
-	# for ing in all_ingredients[:]:
-	# 	for letter in ing:
-	# 		try:
-	# 			letter.encode("ascii")
-	# 		except UnicodeEncodeError:
-	# 			newing = ing.replace(letter, "")
-	# 			all_ingredients[all_ingredients.index(ing)] = newing
+	# Get rid of bullshit unicode characters. Copyright character was causing problems
+	for ing in all_ingredients[:]:
+		for letter in ing:
+			try:
+				letter.encode("ascii")
+			except UnicodeEncodeError:
+				newing = ing.replace(letter, "")
+				all_ingredients[all_ingredients.index(ing)] = newing
 
 	# Write Ingredients to file
 	word = ""
@@ -60,15 +59,15 @@ for c in cuisines:
 
 	print word
 
-	# ing_file = open("allDirections.txt", "a")
-	# ing_file2 = open("Directions.txt", "a")
-	# ing_file3 = open(word + "Directions.txt", "w")
+	ing_file = open("allDirections.txt", "a")
+	ing_file2 = open("dietHealthDirections.txt", "a")
+	ing_file3 = open(word + "Directions.txt", "w")
 
-	# for ing in all_ingredients:
-	# 	ing_file.write(ing + "\n")
-	# 	ing_file2.write(ing + "\n")
-	# 	ing_file3.write(ing + "\n")
+	for ing in all_ingredients:
+		ing_file.write(ing + "\n")
+		ing_file2.write(ing + "\n")
+		ing_file3.write(ing + "\n")
 
-	# ing_file.close()
-	# ing_file2.close()
-	# ing_file3.close()
+	ing_file.close()
+	ing_file2.close()
+	ing_file3.close()
