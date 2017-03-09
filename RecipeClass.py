bcc28f3d7853297b64486535802162e1aa8c204d
Adrian0121 <Adrian@users.noreply.github.com>
2017-03-08 21:00:34 -0600

    add class files

---
+++ RecipeClass.py
@@ -0,0 +1,6 @@
+class Recipe:
+	def __init__(self, ingredients, tools, methods, step_list):
+		self.ingredients = ingredients
+		self.tools = tools
+		self.methods = methods
+		self.steps = step_list
