3c0586e0f0610b1fe050fb65a58cfd3a13db7c51
Drew Bronson <bronsonboy96@gmail.com>
2017-03-12 17:07:20 -0500

    Remove tabs; comment out chdir.

--- recipe.py
+++ recipe.py
@@ -1,7 +1,7 @@
 # execfile('/Users/Omar/Desktop/Code/recipe-parser/recipe.py')
 import os 
-if(os.getcwd() != '/Users/Omar/Desktop/Code/recipe-parser'):
-	os.chdir("../Desktop/Code/recipe-parser")
+#if(os.getcwd() != '/Users/Omar/Desktop/Code/recipe-parser'):
+#    os.chdir("../Desktop/Code/recipe-parser")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Set up & Utility Functions: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import pattern.en
 
@@ -11,91 +11,91 @@ execfile('StepClass.py')
 execfile('RecipeClass.py')
 
 def matchDirnAndIngredientInput(directions, ingredients):
-	#This will match each recipe in the directions & ingredients file
-	#ASSUMING they have already been SegmentedByNextRecipeTag AND they have same # of recipes
-	pairings = []
-	while("__SPLIT__\n" in directions):
-		dirn_end_index = directions.index("__SPLIT__\n")
-		ingr_end_index = ingredients.index("__SPLIT__\n")
-		recipe_dirns = directions[:dirn_end_index]
-		recipe_ingrs = ingredients[:ingr_end_index]
-		
-		pairings.append([recipe_ingrs, recipe_dirns])
-		directions = directions[dirn_end_index+1:]
-		ingredients = ingredients[ingr_end_index+1:]
-	return pairings
+    #This will match each recipe in the directions & ingredients file
+    #ASSUMING they have already been SegmentedByNextRecipeTag AND they have same # of recipes
+    pairings = []
+    while("__SPLIT__\n" in directions):
+        dirn_end_index = directions.index("__SPLIT__\n")
+        ingr_end_index = ingredients.index("__SPLIT__\n")
+        recipe_dirns = directions[:dirn_end_index]
+        recipe_ingrs = ingredients[:ingr_end_index]
+        
+        pairings.append([recipe_ingrs, recipe_dirns])
+        directions = directions[dirn_end_index+1:]
+        ingredients = ingredients[ingr_end_index+1:]
+    return pairings
 
 def getRecipeIngredientsAttributes(recipe_ingrs_raw):
-	recipe_ingrs_formatted = []
-	for ingr in recipe_ingrs_raw:
-		ingr_formatted = parse_ingredient(ingr)
-		recipe_ingrs_formatted.append(ingr_formatted)
-	recipe_ingrs_extracted = [recipe["Ingredient"] for recipe in recipe_ingrs_formatted]
-	return recipe_ingrs_extracted
+    recipe_ingrs_formatted = []
+    for ingr in recipe_ingrs_raw:
+        ingr_formatted = parse_ingredient(ingr)
+        recipe_ingrs_formatted.append(ingr_formatted)
+    recipe_ingrs_extracted = [recipe["Ingredient"] for recipe in recipe_ingrs_formatted]
+    return recipe_ingrs_extracted
 
 def getAdjacentIngredient(lower_step_tokens, index, recipe_ingrs_extracted_tokens):
-	#where recipe_ingrs_extracted_toks = map(lambda(x): nltk.word_tokenize(x), recipe_ingrs_extracted)
-	#& recipe_ingrs_extracted=getRecipeIngredientsAttributes(recipe_ingrs_raw)
-	#Basically, recipe_ingrs_extracted_toks is a TOKENIZED LIST OF ALL OF THE 'INGREDIENTS' - no descriptions, measurements, nothing
-	lower_step_pos = nltk.pos_tag(lower_step_tokens)
+    #where recipe_ingrs_extracted_toks = map(lambda(x): nltk.word_tokenize(x), recipe_ingrs_extracted)
+    #& recipe_ingrs_extracted=getRecipeIngredientsAttributes(recipe_ingrs_raw)
+    #Basically, recipe_ingrs_extracted_toks is a TOKENIZED LIST OF ALL OF THE 'INGREDIENTS' - no descriptions, measurements, nothing
+    lower_step_pos = nltk.pos_tag(lower_step_tokens)
 
-	iterator = index
+    iterator = index
 
-	while( (iterator<len(lower_step_tokens)) and ((lower_step_pos[iterator][1] in ["DT", "CD"]) or (lower_step_tokens[iterator] in ing_measurements) or (lower_step_tokens[iterator] in ing_descriptors) 
-	  or (bool(filter(lambda(x): x.isdigit(), lower_step_tokens[iterator]))))):
-		iterator+=1
+    while( (iterator<len(lower_step_tokens)) and ((lower_step_pos[iterator][1] in ["DT", "CD"]) or (lower_step_tokens[iterator] in ing_measurements) or (lower_step_tokens[iterator] in ing_descriptors) 
+      or (bool(filter(lambda(x): x.isdigit(), lower_step_tokens[iterator]))))):
+        iterator+=1
 
-	if(iterator==len(lower_step_tokens)):
-		return None
+    if(iterator==len(lower_step_tokens)):
+        return None
 
-	susp_ingr  = lower_step_tokens[iterator]
-	for ingr_token in recipe_ingrs_extracted_tokens:
-		#The tokens of 1 ingredient 
-		if(susp_ingr in ingr_token):
-			len_ingr_token = len(ingr_token)
-			iterator_fwd, iterator_bwd = None, None
-			if(len_ingr_token>1):
-				token_index = ingr_token.index(susp_ingr)
-				len_step_tokens = len(lower_step_tokens)
+    susp_ingr  = lower_step_tokens[iterator]
+    for ingr_token in recipe_ingrs_extracted_tokens:
+        #The tokens of 1 ingredient 
+        if(susp_ingr in ingr_token):
+            len_ingr_token = len(ingr_token)
+            iterator_fwd, iterator_bwd = None, None
+            if(len_ingr_token>1):
+                token_index = ingr_token.index(susp_ingr)
+                len_step_tokens = len(lower_step_tokens)
 
-				if(token_index==0): 
-					#if suspected ingredient is found at the start of the token, check if there is a match w the next one
-					iterator_fwd = 1
-					while( (len_step_tokens > (token_index+iterator_fwd) ) and ( lower_step_tokens[token_index+iterator_fwd] in ingr_token) ):
-						iterator_fwd+=1
-					iterator_fwd-=1
+                if(token_index==0): 
+                    #if suspected ingredient is found at the start of the token, check if there is a match w the next one
+                    iterator_fwd = 1
+                    while( (len_step_tokens > (token_index+iterator_fwd) ) and ( lower_step_tokens[token_index+iterator_fwd] in ingr_token) ):
+                        iterator_fwd+=1
+                    iterator_fwd-=1
 
-				elif(token_index == (len_step_tokens-1) ): 
-					#if suspected ingredient is found at the end of the token, check if there is a match w the previous one
-					iterator_bwd = -1
-					while( (len_step_tokens > (token_index+iterator_bwd) ) and ( lower_step_tokens[token_index+iterator_bwd] in ingr_token) ):
-						iterator_bwd-=1
-					iterator_bwd+=1
+                elif(token_index == (len_step_tokens-1) ): 
+                    #if suspected ingredient is found at the end of the token, check if there is a match w the previous one
+                    iterator_bwd = -1
+                    while( (len_step_tokens > (token_index+iterator_bwd) ) and ( lower_step_tokens[token_index+iterator_bwd] in ingr_token) ):
+                        iterator_bwd-=1
+                    iterator_bwd+=1
 
-				else:
-					#may be a chance that it is before OR after.
-					iterator_fwd, iterator_bwd = 1, -1
-					while( (len_step_tokens > (token_index+iterator_fwd) ) and ( lower_step_tokens[token_index+iterator_fwd] in ingr_token) ):
-						iterator_fwd+=1
-					
-					while( ( (token_index+iterator_bwd) >= 0 ) and ( lower_step_tokens[token_index+iterator_bwd] in ingr_token) ):
-						iterator_bwd-=1
-					
-					#Just to undo the offset
-					iterator_fwd-=1
-					iterator_bwd+=1
+                else:
+                    #may be a chance that it is before OR after.
+                    iterator_fwd, iterator_bwd = 1, -1
+                    while( (len_step_tokens > (token_index+iterator_fwd) ) and ( lower_step_tokens[token_index+iterator_fwd] in ingr_token) ):
+                        iterator_fwd+=1
+                    
+                    while( ( (token_index+iterator_bwd) >= 0 ) and ( lower_step_tokens[token_index+iterator_bwd] in ingr_token) ):
+                        iterator_bwd-=1
+                    
+                    #Just to undo the offset
+                    iterator_fwd-=1
+                    iterator_bwd+=1
 
-			if((iterator_fwd!=None) and (iterator_bwd!=None)):
-				return lower_step_tokens[iterator+iterator_bwd : iterator+iterator_fwd+1]
-			elif(iterator_fwd):
-				return lower_step_tokens[iterator: iterator+iterator_fwd+1]
-			elif(iterator_bwd):
-				return lower_step_tokens[iterator+iterator_bwd : iterator]
-			else:
-				return [lower_step_tokens[iterator]]
-		else:
-			continue
-	return None
+            if((iterator_fwd!=None) and (iterator_bwd!=None)):
+                return lower_step_tokens[iterator+iterator_bwd : iterator+iterator_fwd+1]
+            elif(iterator_fwd):
+                return lower_step_tokens[iterator: iterator+iterator_fwd+1]
+            elif(iterator_bwd):
+                return lower_step_tokens[iterator+iterator_bwd : iterator]
+            else:
+                return [lower_step_tokens[iterator]]
+        else:
+            continue
+    return None
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ User Interface: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -106,41 +106,41 @@ recipe_pairings = matchDirnAndIngredientInput(directions, ingredients)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ WorkSpace/Demo-code: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #This would eventually be parameterizable but just for now:
 def runTests(index_pairing=None):
-	if(index_pairing!=None):
-		pairing = recipe_pairings[index_pairing]
-		print "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-		recipe_ingrs_raw, recipe_dirns_raw = pairing
+    if(index_pairing!=None):
+        pairing = recipe_pairings[index_pairing]
+        print "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        recipe_ingrs_raw, recipe_dirns_raw = pairing
 
-		myRecipe = Recipe()
-		myRecipe.ProvideRawIngrData(recipe_ingrs_raw)
-		myRecipe.ProvideRawDirnData(recipe_dirns_raw)
-		myRecipe.extractFullIngrData(store=True)
-		myRecipe.extractSolelyIngrData(store=True)
-		myRecipe.extractSteps(store=True)
-		# print "SolelyIngrData: ", myRecipe.SolelyIngrData, " \n"
-		for step in myRecipe.Steps:
-			# print "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nstep.firstWordAnalysis() returns: ", step.firstWordAnalysis()
-			# print "step.splitAnalysis() returns: ", step.splitAnalysis(),
-			step.ExtractFromTxt(Display=True)
-	else:
-		for pairing in recipe_pairings[0:20]:
-			print "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-			recipe_ingrs_raw, recipe_dirns_raw = pairing
+        myRecipe = Recipe()
+        myRecipe.ProvideRawIngrData(recipe_ingrs_raw)
+        myRecipe.ProvideRawDirnData(recipe_dirns_raw)
+        myRecipe.extractFullIngrData(store=True)
+        myRecipe.extractSolelyIngrData(store=True)
+        myRecipe.extractSteps(store=True)
+        # print "SolelyIngrData: ", myRecipe.SolelyIngrData, " \n"
+        for step in myRecipe.Steps:
+            # print "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nstep.firstWordAnalysis() returns: ", step.firstWordAnalysis()
+            # print "step.splitAnalysis() returns: ", step.splitAnalysis(),
+            step.ExtractFromTxt(Display=True)
+    else:
+        for pairing in recipe_pairings[0:20]:
+            print "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            recipe_ingrs_raw, recipe_dirns_raw = pairing
 
-			myRecipe = Recipe()
-			myRecipe.ProvideRawIngrData(recipe_ingrs_raw)
-			myRecipe.ProvideRawDirnData(recipe_dirns_raw)
-			myRecipe.extractFullIngrData(store=True)
-			myRecipe.extractSolelyIngrData(store=True)
-			myRecipe.extractSteps(store=True)
-			print "SolelyIngrData: ", myRecipe.SolelyIngrData, "\n"
-			for step in myRecipe.Steps:
-				print "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nstep.firstWordAnalysis() returns: ", step.firstWordAnalysis()
-				print "step.splitAnalysis() returns: ", step.splitAnalysis(),
-				step.ExtractFromTxt(Display=True)
-			myRecipe.categorizeMethods(store=True)
-			print "myRecipe.PrimaryMethods = ", myRecipe.PrimaryMethods
-			print "myRecipe.SecondaryMethods = ", myRecipe.SecondaryMethods
+            myRecipe = Recipe()
+            myRecipe.ProvideRawIngrData(recipe_ingrs_raw)
+            myRecipe.ProvideRawDirnData(recipe_dirns_raw)
+            myRecipe.extractFullIngrData(store=True)
+            myRecipe.extractSolelyIngrData(store=True)
+            myRecipe.extractSteps(store=True)
+            print "SolelyIngrData: ", myRecipe.SolelyIngrData, "\n"
+            for step in myRecipe.Steps:
+                print "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nstep.firstWordAnalysis() returns: ", step.firstWordAnalysis()
+                print "step.splitAnalysis() returns: ", step.splitAnalysis(),
+                step.ExtractFromTxt(Display=True)
+            myRecipe.categorizeMethods(store=True)
+            print "myRecipe.PrimaryMethods = ", myRecipe.PrimaryMethods
+            print "myRecipe.SecondaryMethods = ", myRecipe.SecondaryMethods
 
 
 
